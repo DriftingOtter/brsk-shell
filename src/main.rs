3a7df49be5c70901c9586ae89920308fa3478c03
DriftingOtter <dakshkaul7790@gmail.com>
2024-08-09 17:13:51 -0400

    Refactor shell implementation: add users dependency, streamline input handling, and improve directory navigation
    - Added daksh daksh crate dependency in  and .
    - Refactored  to improve prompt handling, directory changes, and command execution.
    - Added  function to handle prompt display.
    - Improved  for better input parsing.
    - Updated  to handle relative and absolute paths more effectively.
    - Removed unused pipe handling code and streamlined command execution logic.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,70 @@
 use std::env;
 use std::ffi::OsStr;
-use std::io;
-use std::io::stdout;
-use std::io::Write;
-use std::process::exit;
-use std::process::Command;
-use std::process::Stdio;
-use std::path::Path;
+use std::io::{self, Write};
+use std::path::{Path, PathBuf};
+use std::process::{exit, Command};
 
 fn main() {
-    loop {
+    println!("brsk shell: v0.0.1 (alpha)");
 
-        // Print command prompt
-        print!(">> ");
-        if let Err(err) = stdout().flush() {
+    loop {
+        if let Err(err) = display_prompt() {
             eprint!("{}", err);
+            continue;
         }
 
-        // Parse user input
         let (command, args) = parse_input();
+        let cwd = env::current_dir().unwrap_or_default();
 
-        // Execute built-in commands
-        if command == "cd" {
-            cd(args);
-
-        } else if command == "exit" {
-            exit(0);
-        } else {
-            // Execute external commands
-            execute_command(&command, args);
+        match command.as_str() {
+            "cd" => change_directory(args, cwd),
+            "exit" => exit(0),
+            _ => execute_command(&command, args),
         }
     }
 }
 
-fn cd(args: Vec<String>) -> bool {
-    let default_directory = "/".to_string();
+fn display_prompt() -> io::Result<()> {
+    print!(">> ");
+    io::stdout().flush()
+}
 
-    let directory = args.get(0).unwrap_or(&default_directory);
-    let root = Path::new(directory);
+fn parse_input() -> (String, Vec<String>) {
+    let mut input = String::new();
 
-    match env::set_current_dir(&root) {
-        Ok(_) => true,
-        Err(err) => {
-            eprintln!("Error changing directory: {}", err);
-            false
+    if let Err(err) = io::stdin().read_line(&mut input) {
+        eprintln!("Error reading input: {}", err);
+        return (String::new(), Vec::new());
+    }
+
+    let trimmed_input = input.trim();
+    let mut tokens = trimmed_input.split_whitespace();
+
+    let command = tokens.next().unwrap_or_default().to_string();
+    let args: Vec<String> = tokens.map(String::from).collect();
+    
+    (command, args)
+}
+
+fn change_directory(args: Vec<String>, cwd: PathBuf) {
+    // Get directory from args || default to home if none provided
+    let target_path = args.get(0).map_or_else(
+        || format!("/home/{}/", env::var("USER").unwrap_or_default()),
+        |loc| {
+            // Absolute path
+            if loc.starts_with('/') {
+                format!("{}{}", cwd.to_str().unwrap_or(""), loc)
+            } else {
+                // Relative path
+                format!("{}/{}", cwd.to_str().unwrap_or(""), loc)
+            }
         }
+    );
+
+    let path = Path::new(&target_path);
+
+    if let Err(err) = env::set_current_dir(&path) {
+        eprintln!("Error changing directory: {}", err);
     }
 }
 
@@ -61,70 +81,3 @@ fn execute_command(command: &str, args: Vec<String>) {
     }
 }
 
-fn parse_input() -> (String, Vec<String>) {
-    let mut input = String::new();
-    if let Err(err) = io::stdin().read_line(&mut input) {
-        eprintln!("Error reading input: {}", err);
-    }
-
-    if check_pipes(input.clone().as_str()) {
-        let mut commands = input.trim().split(" | ").peekable();
-
-        while let Some(command) = commands.next() {
-            let mut parts = command.trim().split_whitespace();
-            let command   = parts.next().unwrap();
-            let args      = parts;
-
-            let mut prev_command = None;
-
-            let stdin = prev_command
-                .map_or(Stdio::inherit(), |output: std::process::Child| Stdio::from(output.stdout.unwrap()));
-
-            let stdout = if commands.peek().is_some() {
-                Stdio::piped()
-            } else {
-                Stdio::inherit()
-            };
-
-            let output = Command::new(command)
-                .args(args)
-                .stdin(stdin)
-                .stdout(stdout)
-                .spawn();
-
-            match output {
-                        Ok(output) => { prev_command = Some(output); },
-                        Err(e) => {
-                            prev_command = None;
-                            eprintln!("{}", e);
-                        },
-                    };
-
-            if let Some(mut final_command) = prev_command {
-                match final_command.wait() {
-                    Ok(_) => {},
-                    Err(err) => eprintln!("{}", err),
-                }
-            }
-        }
-    }
-
-    // Parse input into command name and arguments
-    let mut input_tokens = input.trim().split_whitespace();
-
-    let command = input_tokens.next().unwrap_or_default().to_string();
-    let args: Vec<String> = input_tokens.map(|s| s.to_string()).collect();
-
-    (command, args)
-}
-
-fn check_pipes(input: &str) -> bool {
-    let trimmed = input.trim();
-    let segments: Vec<&str> = trimmed.split(" | ").collect();
-
-    if segments.len() >= 2 {
-        return true;
-    } else {
-        return false;
-    }
-}
