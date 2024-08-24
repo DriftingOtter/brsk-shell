cf789b6c20baf24b5934800d31e8639e7a6214b4
DriftingOtter <dakshkaul7790@gmail.com>
2024-08-24 13:36:30 -0400

    UPGRADE: Added Capture && Handling for SIGINT, SIGKILL, SIGTERM.

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,68 @@
 use std::env;
+use std::thread;
 use std::fs::{self, OpenOptions};
 use std::ffi::OsStr;
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 use std::process::{exit, Command};
 use std::time::{SystemTime, UNIX_EPOCH};
+use signal_hook::{consts::SIGINT, iterator::Signals};
 
 fn main() {
+    // capture SIGINT && Other Unix Signals
+    set_signal_tap();
 
+    // init text
     println!("brsk shell: v0.0.1 (alpha)");
 
-    // creating log file for command history
+    // Creating log file for command history
     let mut log_path: String = String::new();
+    let mut rc_path:  String = String::new();
 
-    if find_history_log().is_some() {
-        log_path = find_history_log().unwrap();
-    } else {
-        match create_log_file() {
-            Some(path) => log_path = path,
-            None    => {
-                eprintln!("Could not creat log file");
+    // Find/create log file (.brsk_history)
+    log_path = match find_history_log() {
+        Some(path) => path,
+        None => match create_log_file() {
+            Some(path) => path,
+            None => {
+                eprintln!("Could not create log file");
+                return;
             },
-        };
-    }
+        },
+    };
+
+    // Find/create rc file (.brskrc)
+    rc_path = match find_rc_file() {
+        Some(path) => path,
+        None => match create_rc_file() {
+            Some(path) => path,
+            None => {
+                eprintln!("Could not create .brskrc file");
+                return;
+            },
+        },
+    };
 
     loop {
-
-        // display entry prompt
+        // Display entry prompt
         if let Err(err) = display_prompt() {
             eprint!("{}", err);
             continue;
         }
 
-        // read user input
+        // Read user input
         let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => {},
-            Err(err) => eprintln!("{}", err),
-        };
+        if let Err(err) = io::stdin().read_line(&mut input) {
+            eprintln!("{}", err);
+            continue;
+        }
 
-        // parse user input into a Vector or tuples of its parts
+        // Parse user input into a vector of command tuples
         let command_queue = parse_input(input.clone());
+
         let mut return_code: isize = 0;
 
-        // run execution on all vectored commands
+        // Run execution on all vectored commands
         for (command, args, input) in command_queue {
             match command.as_str() {
                 "cd" => {
@@ -84,7 +103,7 @@ fn parse_input(input: String) -> Vec<(String, Vec<String>, String)> {
         let command = tokens.next().unwrap_or_default().to_string();
         let args: Vec<String> = tokens.map(String::from).collect();
 
-        // We can use the original input as a placeholder for the third part
+        // Use the original input as a placeholder for the third part
         command_list.push((command, args, input.clone()));
     }
 
@@ -134,6 +153,7 @@ fn execute_command(command: &str, args: Vec<String>) -> Option<isize> {
 fn find_history_log() -> Option<String> {
     let default_log_path: String = format!("/home/{}/.brsk_history", 
         env::var("USER").unwrap().trim());
+
     match fs::metadata(default_log_path.clone()) {
         Ok(_)  => return Some(default_log_path),
         Err(_) => return None,
@@ -144,9 +164,8 @@ fn create_log_file() -> Option<String> {
     let default_log_path: String = format!("/home/{}/.brsk_history", 
         env::var("USER").unwrap().trim());
 
-
     match fs::write(default_log_path.clone(), "") {
-        Ok(_) => return Some(default_log_path),
+        Ok(_)  => return Some(default_log_path),
         Err(_) => return None,
     }
 }
@@ -157,15 +176,61 @@ fn create_log(log_path: &str, command: &str, return_code: isize) -> Option<()> {
         .unwrap()
         .as_secs();
 
+    // credit to zsh_history for this format
     let log = format!("{},{},{}", unix_time, return_code, command);
 
     match OpenOptions::new().append(true).open(log_path) {
         Ok(mut file) => {
             if let Err(_) = file.write_all(log.as_bytes()) {
                 return None;
+            } else {
+                return Some(());
             }
-            return Some(());
         }
-        Err(_) => None,
+        Err(_) => return None,
     }
 }
+
+fn find_rc_file() -> Option<String> {
+    let default_log_path: String = format!("/home/{}/.brskrc", 
+        env::var("USER").unwrap().trim());
+    match fs::metadata(default_log_path.clone()) {
+        Ok(_)  => return Some(default_log_path),
+        Err(_) => return None,
+    }
+}
+
+fn create_rc_file() -> Option<String> {
+    let default_log_path: String = format!("/home/{}/.brskrc", 
+        env::var("USER").unwrap().trim());
+
+    match fs::write(default_log_path.clone(), "") {
+        Ok(_)  => return Some(default_log_path),
+        Err(_) => return None,
+    }
+}
+
+fn set_signal_tap() {
+    let mut signals = Signals::new([SIGINT]).unwrap();
+
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            match sig {
+                SIGINT => {
+                    println!("");
+                    exit(0);
+                }
+                15 => {
+                    println!("");
+                    return;
+                },
+                9  => {
+                    println!("");
+                    exit(0);
+                },
+                _  => exit(-1),
+            }
+        }
+    });
+}
+
